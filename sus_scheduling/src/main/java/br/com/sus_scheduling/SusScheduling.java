43e1e350199ddf0b6a6c992a893773bf8ce29bff
Guilherme Lira <guilira.dev@gmail.com>
2025-09-22 18:10:53 -0300

    feature: adicionando dependencia do Redis

--- sus_scheduling/src/main/java/br/com/sus_scheduling/SusScheduling.java
+++ sus_scheduling/src/main/java/br/com/sus_scheduling/SusScheduling.java
@@ -2,12 +2,14 @@ package br.com.sus_scheduling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
+@EnableCaching
 public class SusScheduling {
 
 	public static void main(String[] args) {
